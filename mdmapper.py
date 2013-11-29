e70cc9808f87b23dd225fefb050d86395807c972
geobretagne-template <fabrice@phung.fr>
2013-11-29 13:11:03 +0100

    usage: mdmapper.py [-h] [-o filepath] [-e xmin,ymin,xmax,ymax] [-m maxrecords]                    [--http_proxy HTTP_PROXY]                    csw
    geOrchestra metadata mapper. Queries a CSW service, retrieves dataset bboxes,
    produces a georeferenced table of the datasets.
    
    positional arguments:
      csw                   the CSW service URL, default
                            http://sdi.georchestra.org/geonetwork/srv/eng/csw
    
    optional arguments:
      -h, --help            show this help message and exit
      -o filepath, --output filepath
                            shapefile file path, default "metadata.shp"
      -e xmin,ymin,xmax,ymax, --extent xmin,ymin,xmax,ymax
                            results are contained in WGS84 extent, default
                            "-180,-90,180,90"
      -m maxrecords, --maxrecords maxrecords
                            retrieves no more than MAXRECORDS metadatas, default
                            500
      --http_proxy HTTP_PROXY
                            http proxy host and port, example http://proxy:3128

--- mdmapper.py
+++ mdmapper.py
@@ -1,24 +1,86 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import shapefile, urllib2
+import shapefile, urllib2, argparse, logging
 from owslib.csw import CatalogueServiceWeb
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-# catalog URL
-cswUrl = "http://geobretagne.fr/geonetwork/srv/fre/csw"
-# bbox filter
-bbox = [-4.8,47.3,-0.9,49]
-#~ proxy = "http://127.0.0.1:8888"
-proxy = ""
+# default parameters
+default = {
+    'csw': 'http://sdi.georchestra.org/geonetwork/srv/eng/csw',
+    'extent': '-180,-90,180,90',
+    'maxrecords': 500,
+    'output': 'metadata.shp'
+}
 
-if bool(proxy):
-    proxyHandler = urllib2.ProxyHandler({"http" : proxy})
+
+def cleanAttr(s):
+    """
+    Converts string to unicode string
+    """
+    return s.encode('utf-8') if bool(s) else ''
+
+##
+## arguments retrieval
+##
+
+parser = argparse.ArgumentParser(description="""geOrchestra metadata mapper.
+Queries a CSW service, retrieves dataset bboxes,
+produces a georeferenced table of the datasets.""")
+
+parser.add_argument('csw',
+    type=str,
+    default=default['csw'] ,
+    help='the CSW service URL, default '+default['csw'])
+
+# shapefile name
+parser.add_argument('-o', '--output',
+    type=str,
+    default=default['output'],
+    help='shapefile file path, default "%s"'%default['output'],
+    metavar='filepath')
+
+# optional extent restriction
+parser.add_argument('-e', '--extent',
+    type=str,
+    default=default['extent'],
+    help='results are contained in WGS84 extent, default "%s"'%default['extent'],
+    metavar='xmin,ymin,xmax,ymax')
+
+# optional max records
+parser.add_argument('-m', '--maxrecords',
+    type=int,
+    default=500,
+    help='retrieves no more than MAXRECORDS metadatas, default %s'%default['maxrecords'],
+    metavar='maxrecords')
+
+# optional http proxy
+parser.add_argument('--http_proxy', type=str,
+    default=None,
+    help='http proxy host and port, example http://proxy:3128')
+
+args = parser.parse_args()
+
+
+# optional proxy handler for urllib2 and owslib
+if bool(args.http_proxy):
+    logging.debug('using proxy %s'%args.http_proxy)
+    proxyHandler = urllib2.ProxyHandler({"http" : args.http_proxy})
     opener = urllib2.build_opener(proxyHandler)
     urllib2.install_opener(opener)
 
-csw = CatalogueServiceWeb(cswUrl)
-csw.getrecords(qtype="dataset", keywords=[], esn="full", maxrecords=1000, bbox=bbox)
+# csw connection
+bbox = [float(x) for x in args.bbox.split(',')]
+logging.debug('connecting to %s'%args.csw)
+csw = CatalogueServiceWeb(args.csw)
+logging.info('retrieving %s datasets, bbox=%s'%(args.maxrecords, bbox))
+csw.getrecords(qtype="dataset", keywords=[], esn="full", maxrecords=args.maxrecords, bbox=bbox)
+logging.info('retrieved %s records'%len(csw.records))
+if len(csw.records)>=args.maxrecords:
+    logging.warn('maxrecords reached')
 
+
+# shapefile preparation
 shp = shapefile.Writer(shapefile.POLYGON)
 shp.autoBalance = 1
 shp.field('id')
@@ -30,23 +92,15 @@ shp.field('subjects')
 
 layercount = 0
 
-# owslib can access these fields :
-#'abstract', 'accessrights', 'alternative', 'bbox', 'contributor',
-#'coverage', 'created', 'creator', 'date', 'format', 'identifier',
-#'identifiers', 'ispartof', 'issued', 'language', 'license', 'modified',
-# 'publisher', 'references', 'relation', 'rights', 'rightsholder', 'source',
-#'spatial', 'subjects', 'temporal', 'title', 'type', 'uris', 'xml'
-
-
-def cleanAttr(s):
-    """
-    Converts string to unicode string
-    """
-    return s.encode('utf-8') if bool(s) else ''
-
 for mdId in csw.records:
+    logging.debug('reading %s'%mdId)
+    # owslib can access these fields :
+    #'abstract', 'accessrights', 'alternative', 'bbox', 'contributor',
+    #'coverage', 'created', 'creator', 'date', 'format', 'identifier',
+    #'identifiers', 'ispartof', 'issued', 'language', 'license', 'modified',
+    # 'publisher', 'references', 'relation', 'rights', 'rightsholder', 'source',
+    #'spatial', 'subjects', 'temporal', 'title', 'type', 'uris', 'xml'
     rec = csw.records[mdId]
-
     title = cleanAttr(rec.title)
     abstract = cleanAttr(rec.abstract)
     date = cleanAttr(rec.date)
@@ -72,10 +126,11 @@ for mdId in csw.records:
                 layerurl = cleanAttr(uri['url']) if uri['url'] else 'misconfigured layer url'
                 layertitle = cleanAttr(uri['description']) if uri['description'] else 'misconfigured layer title'
                 shp.record(mdId, title, layerurl, layername, layertitle, subjects)
+                logging.debug('WMS layer found: %s'%layername)
                 layercount = layercount + 1
 
-print "layers %s"%layercount
-print "md %s"%len(csw.records)
-
+logging.info('metadatas found: %s'%len(csw.records))
+logging.info('layers found: %s'%layercount)
+logging.info('saving results in: %s'%args.output)
 shp.save("metadata.shp")
 
